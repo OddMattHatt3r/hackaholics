1cc47835a221073fe81b5af4b0cdeed40666fa74
OddMattHatt3r <67079190+OddMattHatt3r@users.noreply.github.com>
2025-03-22 11:56:39 -0400

    Update civAI.py

--- civAI.py
+++ civAI.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request, abort,jsonify,session,flash,Response,send_file,make_response, g
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "HACKAHOLICS1234"
 
 @app.route('/')
 def home():
